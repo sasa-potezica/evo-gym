4a089324594d124a0d2104341af8413f90f43833
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-18 20:11:25 +0100

    Add homepage components and animations

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import SponsorRedBull from '@/assets/SponsorRedBull.png'
 import SponsorForbes from '@/assets/SponsorForbes.png'  
 import SponsorFortune from '@/assets/SponsorFortune.png' 
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { motion } from 'framer-motion'
 
 
 type Props = {
@@ -19,34 +20,57 @@ const index = ({setSelectedPage}: Props) => {
 
   return (
     <section id='home' className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'>
-        <div>
-            <div>
-                <div>
-                    <div>
+        <motion.div
+        onViewportEnter={()=>setSelectedPage(SelectedPage.Home)} 
+        className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
+            <div className='z-10 mt-32 md:basis-3/5'>
+                <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{once: true, amount: 0.5}}
+                transition={{duration: 0.5}}
+                variants={{
+                    hidden: {opacity: 0, x: -50},
+                    visible: {opacity: 1, x: 0},
+                }}
+                 className='md:-mt-20'>
+                    <div className='relative'>
+                        <div className='before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext'>
                         <img src={HomePageText} alt="home-page-text" />
+                        </div>
                     </div>
-                </div>
-                <p>
-                    Unrivaled Gym. Unparalleled Training Fitness Classes. World Class Studios to get the Body Shapes That you Dream of.. Get Your Dream Body Now.
-                </p>
-            </div>
-            <div>
+                    <p className='mt-8 text-sm'>
+                        Unrivaled Gym. Unparalleled Training Fitness Classes. World Class Studios to get the Body Shapes That you Dream of.. Get Your Dream Body Now.
+                    </p>
+                </motion.div>
+            <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once: true, amount: 0.5}}
+            transition={{delay: 0.2, duration: 0.5}}
+            variants={{
+                hidden: {opacity: 0, x: -50},
+                visible: {opacity: 1, x: 0},
+            }}
+             className='mt-8 flex items-center gap-8 md:justify-start'>
                 <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
                 <AnchorLink className='text-sm font-bold text-primary-500 underline hover:text-secondary-500'
                 onClick={()=>setSelectedPage(SelectedPage.ContactUs)}
                 href={`#${SelectedPage.ContactUs}`}>
                     <p>Learn More</p>
                 </AnchorLink>
-            </div>
-        <div>
-           <img src={HomePageGraphic} alt="home-page-graphic" />
-        </div>
+            </motion.div>
         </div>
 
+        <div className='flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end'>
+           <img src={HomePageGraphic} alt="home-page-graphic" />
+        </div>
+    </motion.div>
+
         {isAboveMediumScreens && (
-            <div>
-                <div>
-                    <div>
+            <div className='h-[160px] w-full bg-primary-100 py-10'>
+                <div className='mx-auto w-5/6'>
+                    <div className='flex w-3/5 items-center justify-between gap-8'>
                         <img src={SponsorRedBull} alt="redbull-sponsor" />
                         <img src={SponsorForbes} alt="forbes-sponsor" />
                         <img src={SponsorFortune} alt="fortune-sponsor" />
