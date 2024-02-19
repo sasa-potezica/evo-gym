7ace5a5f6dd2f50dc33d195471b9ea9ba2fbdf91
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-19 19:37:34 +0100

    Set Up Benefits Component

---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,15 @@
+import { SelectedPage } from "@/shared/types"
+import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid"
+type Props = {
+    setSelectedPage: (value: SelectedPage)=>void;
+}
+
+const Benefits = ({setSelectedPage}: Props) => {
+  return (
+    <section id='benefits' className="mx-auto min-h-full w-5/6 py-20">
+        
+    </section>
+  )
+}
+
+export default Benefits
