51eec79e385818cd23745eb4ecbc4c780e195910
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-20 15:10:33 +0100

    Added Benefits Section with Dynamic Content Rendering

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -71,26 +71,43 @@ const Benefits = ({setSelectedPage}: Props) => {
                 ))}
             </motion.div>
 
-            <div>
+            <div className="mt-16 items-center justify-between gap-20 md:mt-28 md:flex">
                 <img 
                 className="mx-auto"
                 src={BenefitsPageGraphic} alt="benefits-page-graphic" />
                 <div>
                     <div className="relative">
                         <div className="before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractwaves">
-                            <div>
+                            <motion.div
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{once: true, amount: 0.5}}
+                            transition={{duration: 0.5}}
+                            variants={{
+                                hidden: {opacity: 0, x: 50},
+                                visible: {opacity: 1, x: 0},
+                            }}>
                                 <HText>
                                     MILLIONS OF HAPPY MEMBER GETTING{' '}
                                     <span className="text-primary-500">FIT</span>
                                 </HText>
-                            </div>
+                            </motion.div>
                         </div>
                     </div>
 
-                    <div>
+                    <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{once: true, amount: 0.5}}
+                    transition={{delay: 0.2, duration: 0.5}}
+                    variants={{
+                        hidden: {opacity: 0, x: 50},
+                        visible: {opacity: 1, x: 0},
+                    }}
+                    >
                         <p className="my-5">At our gym, we pride ourselves on our diverse community of members who have found joy and success in their fitness journeys. With millions of happy individuals achieving their fitness goals, our gym stands as a beacon of empowerment and transformation. From weight loss milestones to strength gains, our members experience a myriad of benefits that extend far beyond physical appearance. Our supportive environment fosters camaraderie, ensuring that everyone feels welcomed and encouraged to push their limits.</p>
                         <p className="mb-5">Joining our gym means becoming a part of a dynamic network of individuals who share a common goal: improving their health and well-being. With access to state-of-the-art facilities and expert guidance from our certified trainers, our members embark on a path of self-discovery and personal growth. Whether it's shedding pounds, building muscle, or enhancing overall wellness, our gym provides the resources and support needed to make fitness a fulfilling and sustainable lifestyle. Experience the satisfaction of joining millions of happy members who are not just getting fit, but thriving in every aspect of their lives.</p>
-                    </div>
+                    </motion.div>
 
                     <div className="relative my-16">
                         <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
