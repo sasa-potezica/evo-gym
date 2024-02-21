e6540300a11a78dd51b8b8a82898c2978c154be3
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-21 08:19:47 +0100

    Added form submission handler to process user input.

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -8,6 +8,21 @@ type Props = {
     setSelectedPage: (value: SelectedPage)=>void
 }
 
+const inputStyles = `w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`;
+
+const{
+ register,
+ trigger,
+ formState: {errors}
+} = useForm();
+
+const onSubmit = async (e:any)=>{
+    const isValid = await trigger();
+    if(!isValid){
+        e.preventDefault();
+    }
+}
+
 const ContactUs = ({setSelectedPage}: Props) => {
   return (
     <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
@@ -29,7 +44,36 @@ const ContactUs = ({setSelectedPage}: Props) => {
                 <p className="my-5">Join now to start your fitness journey with us! Our top-notch trainers and equipment are here to help you reach your goals. Don't wait – sign up today!</p>
             </motion.div>
             <div className="mt-10 justify-between gap-8 md:flex">
-
+                <motion.div 
+                initial="hidden"
+                whileInView="visible"
+                viewport={{once: true, amount: 0.5}}
+                transition={{duration: 0.5}}
+                variants={{
+                    hidden: {opacity: 0, y: 50},
+                    visible: {opacity: 1, y: 0},
+                }}
+                className="mt-10 basis-3/5 md:mt-0">
+                    <form target="_blank"
+                    onSubmit={onSubmit}
+                    action="https://formsubmit.co/el/wovate"
+                    method="POST">
+                        <input 
+                        type="text"
+                        placeholder="NAME"
+                        {...register("name", {
+                            required: true,
+                            maxLength: 100,
+                        })}
+                        className={inputStyles}/>
+                        {errors.name &&(
+                            <p className="mt-1 text-primary-500">
+                                {errors.name.type === 'required' && 'This field is required.'}
+                                {errors.name.type === "maxLength" && 'This field is required.'}
+                            </p>
+                        )}
+                    </form>
+                </motion.div>
             </div>
         </motion.div>
     </section>
